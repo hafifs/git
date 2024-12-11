a643aa2935f983d4c696ceb40b2f81caf6ccdd2c
bagongnet.kayen <bagongnet.kayen@gmail.com>
2024-12-11 12:09:59 +0000

    fix: dropdown menu close

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const $triggerEl = document.getElementById('dropdownNavbarLink');
 $triggerEl.addEventListener("click", () => {
     $targetEl.classList.toggle("hidden");
 })
+
+window.onclick = (event) => {
+    if (!event.target.matches('#dropdownNavbarLink')) {
+        $targetEl.classList.add("hidden");
+    }
+}
